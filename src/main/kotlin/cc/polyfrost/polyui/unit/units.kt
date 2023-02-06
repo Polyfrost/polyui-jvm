073c7ffc7138e2635c848dfcdb767c9505d815e7
nxtdaydelivery <12willettsh@gmail.com>
2023-02-06 20:05:13 +0000

    fileheader

--- src/main/kotlin/cc/polyfrost/polyui/unit/units.kt
+++ src/main/kotlin/cc/polyfrost/polyui/unit/units.kt
@@ -1,3 +1,12 @@
+/*
+ * This file is part of PolyUI.
+ * Copyright (C) 2022-2023 Polyfrost and its contributors.
+ * All rights reserved.
+ * PolyUI - simple, easy to use and lightweight UI framework https://polyfrost.cc https://github.com/Polyfrost/polui-jvm
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
 @file:Suppress("NOTHING_TO_INLINE")
 
 package cc.polyfrost.polyui.unit
--- src/main/kotlin/cc/polyfrost/polyui/utils/arrays.kt
+++ src/main/kotlin/cc/polyfrost/polyui/utils/arrays.kt
@@ -1,3 +1,12 @@
+/*
+ * This file is part of PolyUI.
+ * Copyright (C) 2022-2023 Polyfrost and its contributors.
+ * All rights reserved.
+ * PolyUI - simple, easy to use and lightweight UI framework https://polyfrost.cc https://github.com/Polyfrost/polui-jvm
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
 /** This file contains various utilities for arrays and arraylists. */
 @file:Suppress("ReplaceManualRangeWithIndicesCalls", "ReplaceSizeZeroCheckWithIsEmpty")
 
