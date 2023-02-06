f339014dea41d57135b117c43655102a1a84c19b
nxtdaydelivery <12willettsh@gmail.com>
2023-02-06 20:05:49 +0000

    fileheader

--- nanovg-impl/src/main/kotlin/cc/polyfrost/polyui/renderer/impl/NVGRenderer.kt
+++ nanovg-impl/src/main/kotlin/cc/polyfrost/polyui/renderer/impl/NVGRenderer.kt
@@ -1,3 +1,12 @@
+/*
+ * This file is part of PolyUI.
+ * Copyright (C) 2022-2023 Polyfrost and its contributors.
+ * All rights reserved.
+ * PolyUI - simple, easy to use and lightweight UI framework https://polyfrost.cc https://github.com/Polyfrost/polui-jvm
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
 package cc.polyfrost.polyui.renderer.impl
 
 import cc.polyfrost.polyui.PolyUI
