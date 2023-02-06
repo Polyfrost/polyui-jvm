4131fb2248b77e71cc85a639e87f257a0b2a6247
Wyvest <45589059+Wyvest@users.noreply.github.com>
2023-02-06 16:09:12 -0500

    update file header

--- src/main/kotlin/cc/polyfrost/polyui/layout/impl/FlexLayout.kt
+++ src/main/kotlin/cc/polyfrost/polyui/layout/impl/FlexLayout.kt
@@ -2,7 +2,7 @@
  * This file is part of PolyUI.
  * Copyright (C) 2022-2023 Polyfrost and its contributors.
  * All rights reserved.
- * PolyUI - simple, easy to use and lightweight UI framework https://polyfrost.cc https://github.com/Polyfrost/polui-jvm
+ * PolyUI - Fast and lightweight UI framework https://polyfrost.cc https://github.com/Polyfrost/polui-jvm
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  */
