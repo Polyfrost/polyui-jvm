8336bb74c9c75dd25ddf31ead321133fb99e7274
nextdayy <79922345+nextdayy@users.noreply.github.com>
2025-08-06 18:45:53 +0100

    revert text scaling

--- src/main/kotlin/org/polyfrost/polyui/component/impl/Text.kt
+++ src/main/kotlin/org/polyfrost/polyui/component/impl/Text.kt
@@ -230,7 +230,8 @@ open class Text(text: Translator.Text, font: Font? = null, fontSize: Float = 12f
 
     override fun rescale0(scaleX: Float, scaleY: Float, withChildren: Boolean) {
         super.rescale0(scaleX, scaleY, withChildren)
-        fontSize = (height - (spacing * (lines.size - 1))) / lines.size
+//        fontSize = (height - (spacing * (lines.size - 1))) / lines.size
+        fontSize *= scaleY
         // asm: fontSize would normally call this method, but if it isn't as it doesn't change, we need to do it manually
         if (initialized && scaleY == 1f) updateTextBounds(renderer)
     }
